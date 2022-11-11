496f069a5fd7cb7f4dc77d8fb5a1cf91ec43968c
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-11 20:42:37 +0200

    Encapsulation

---
+++ Encapsulation Lab/PersonsInfo/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstname;
+        private string lastname;
+        private int age;
+        private decimal salary;
+        public string FirstName 
+        {
+            get { return this.firstname; } 
+            private set
+            {
+                if(value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastname; }
+            private set
+            {
+                if(value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastname = value;
+            }
+        }
+        public int Age 
+        { 
+            get { return age; }
+            private set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+
+            }
+        }
+
+        public decimal Salary 
+        {
+            get { return salary; }
+            private set
+            {
+                if(value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            }
+        }
+
+        public Person(string firstname, string lastname, int age, decimal salary)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += Salary * percentage/100;
+            }
+            else
+            {
+                Salary += Salary * percentage/200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ Encapsulation Lab/PersonsInfo/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for(int i = 0; i < lines; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(' ');
+                try
+                {
+                    Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+                    people.Add(person);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+               
+
+            }
+            var percent = decimal.Parse(Console.ReadLine());
+            people = people.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList();
+            people.ForEach(p => p.IncreaseSalary(percent));
+            //people.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Team team = new Team("SoftUni");
+
+            foreach(Person person in people)
+            {
+                team.AddPlayer(person);
+            }
+                
+
+        }
+    }
+}
