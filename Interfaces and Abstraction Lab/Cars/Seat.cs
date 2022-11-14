5a327fcff34a52995c70a7c6a409d98968976653
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-14 21:48:13 +0200

    Interfaces and Abstraction

---
+++ Interfaces and Abstraction Lab/Cars/Seat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; private set; }
+
+        public string Color { get; private  set; }
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Seat {this.Model}");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+            return sb.ToString().TrimEnd();
+        }// override string ToString()
+    }
+}
---
+++ Interfaces and Abstraction Lab/Cars/Tesla.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IELectricCar
+    {
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+        public int Battery { get; private set; }
+
+        public Tesla(string model, string color, int batery)
+        {
+            Model = model;
+            Color = color;
+            Battery = batery;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries ");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+            return sb.ToString().TrimEnd();
+        }// override string ToString()
+
+
+    }
+}
