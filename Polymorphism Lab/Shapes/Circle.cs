33778c7ca40c390f8487d408c54629832bdcccf5
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-20 16:45:27 +0200

    Polymorphism Lab

---
+++ Polymorphism Lab/Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+    }
+}
---
+++ Polymorphism Lab/Shapes/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+           return 2*(Height + Width);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+    }
+
+}
