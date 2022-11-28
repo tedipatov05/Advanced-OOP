b3422d0d04d213ffb286e39e7593a9d9c14ad615
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-28 16:23:39 +0200

    Unit testing lab

---
+++ Unit Testing Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void ShouldDummyLoosesHealthIfAttacked()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            dummy.TakeAttack(1);
+            Assert.That(dummy.Health, Is.EqualTo(9));
+        }
+        [Test]
+        public void ShouldDeadDummyThrowAnExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(1));
+        }
+        [Test]
+        public void ShouldDeadDummyCanGiveXP()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.That(() => dummy.GiveExperience(), Is.EqualTo(10));
+        }
+        [Test]
+        public void ShouldAliveDummyCantGiveXP()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+    }
+}
